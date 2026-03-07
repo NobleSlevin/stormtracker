d9dba1d16efab4f555e9339dde3fb9687c467992
Trey <126695003+NobleSlevin@users.noreply.github.com>
2026-03-07 12:49:31 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stormwatch-v52';
+const CACHE = 'stormwatch-v53';
 const ASSETS = ['/', '/index.html', '/app.js', '/icon.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
